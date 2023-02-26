9eded8f9da8af329a3d704b75d18d942800d0a88
Ved Prakash Nautiyal <vedprakashnautiyal@Air-Internet.local>
2023-02-26 20:28:26 +0530

    Added Problem Number 215 (Medium)

---
+++ 215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,10 @@
+//-------------------------------------------------------------My Approach
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort (nums.begin(),nums.end());
+        return nums[nums.size()-k];
+        
+    }
+};
+

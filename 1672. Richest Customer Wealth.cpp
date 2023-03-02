09bb2fa03c4f3e667d6b271f13b29174b8d76c90
Ved Prakash Nautiyal <vedprakashnautiyal@Air-Internet.local>
2023-03-02 21:08:34 +0530

    Added Problem Number 1672 (Easy)

---
+++ 1672. Richest Customer Wealth.cpp
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------My Approach
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        vector <int> ans;
+        for (int i=0;i<accounts.size();i++){
+            int sum=0;
+            for (int j=0;j<accounts[i].size();j++){
+                sum+=accounts[i][j];
+            }
+            ans.push_back(sum);
+        }
+        sort(ans.begin(),ans.end());
+        return ans[ans.size()-1];
+    }
+};
+
+//---------------------------------------------------------------Best Approach (Optimised My Approach)
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        vector <int> ans;
+        int n=accounts.size();
+        int m=accounts[0].size();
+        for (int i=0;i<n;i++){
+            int sum=0;
+            for (int j=0;j<m;j++){
+                sum+=accounts[i][j];
+            }
+            ans.push_back(sum);
+        }
+        return *max_element(ans.begin(),ans.end());
+    }
+};

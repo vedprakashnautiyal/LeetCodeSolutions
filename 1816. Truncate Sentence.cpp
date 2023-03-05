3b5dcaaab7559d1a53f1eadd791b1baaf05df0e1
Ved Prakash Nautiyal <vedprakashnautiyal@Air-Internet.local>
2023-03-05 19:49:34 +0530

    Added Problem Number 1816 (Easy)

--- 1816. Truncate Sentence.cpp
+++ 1816. Truncate Sentence.cpp
@@ -28,3 +28,26 @@ public:
         return ans;
     }
 };
+
+
+//---------------------------------------------------------Best  Approach
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int len = s.size();
+        int count = 0;
+        for(int i=0;i<len;i++)
+        {
+            if(s[i]==' ')
+            {
+                count++;
+            }
+            if(count==k)
+            {
+                s.erase(s.begin()+i,s.end());
+                break;
+            }
+        }
+        return s;
+    }
+};
